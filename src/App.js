7689e097aa1953eb0e8c32ceb98b7461b9666d0e
Indexyz <jiduye@gmail.com>
2018-05-24 14:11:23 +0800

    feat: finish impl

--- src/App.js
+++ src/App.js
@@ -4,17 +4,43 @@ import Footbar from './Footbar'
 import PingCard from './PingCard'
 import { Row, Col } from 'react-flexbox-grid'
 import { Layout, Input, Button, InputNumber } from 'antd'
+import uuid from 'uuid'
 import './App.css'
 
 class App extends Component {
     state = {
-        'hosts': [{
+        'hosts': [],
+        'input': {
             'host': 'www.baidu.com',
             'port': 443,
-        }, {
-            'host': 'www.google.com',
-            'port': 443,
-        }],
+        },
+    }
+
+    addHost() {
+        this.setState(pre => {
+            pre.hosts = [Object.assign({
+                'uuid': uuid.v4(),
+            }, this.state.input), ...pre.hosts]
+            return pre
+        })
+    }
+
+    updateHost(event) {
+        const value = event.target.value
+
+        this.setState(pre => {
+            pre.input.host = value
+            return pre
+        })
+    }
+
+    updatePort(event) {
+        const value = event.target.value
+
+        this.setState(pre => {
+            pre.input.port = value
+            return pre
+        })
     }
 
     render() {
@@ -24,13 +50,25 @@ class App extends Component {
                 <Layout className="siteContext">
                     <Layout.Content style={{ padding: '16px 32px' }}>
                         <Input.Group compact>
-                            <Input placeholder="Host" style={{ width: '70%' }} defaultValue="www.baidu.com" />
-                            <InputNumber placeholder="Port" style={{ width: '15%' }} defaultValue="443" />
-                            <Button style={{ width: '15%' }} type="primary">Add</Button>
+                            <Input
+                                value={this.state.input.host}
+                                onChange={this.updateHost.bind(this)}
+                                placeholder="Host"
+                                style={{ width: '70%' }} />
+                            <InputNumber
+                                value={this.state.input.port}
+                                onChange={this.updatePort.bind(this)}
+                                placeholder="Port"
+                                style={{ width: '15%' }} />
+                            <Button
+                                onClick={this.addHost.bind(this)}
+                                style={{ width: '15%' }}
+                                type="primary">
+                            Add</Button>
                         </Input.Group>
                         <Row>
-                            {this.state.hosts.map((item, i) => {
-                                return (<Col key={i} xs={12} sm={6} md={6} lg={4} style={{ padding: '16px' }}>
+                            {this.state.hosts.map(item => {
+                                return (<Col key={item.uuid} xs={12} sm={6} md={6} lg={4} style={{ padding: '16px' }}>
                                     <PingCard host={item.host} port={item.port} />
                                 </Col>)
                             })}
