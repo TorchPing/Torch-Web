063b9f83759e2075d2b4e3b022eddd929bd0291c
Travis Automatically Bot (from Travis CI) <jiduye@gmail.com>
2019-11-28 01:34:04 +0000

    Deploy TorchPing/Torch-Web to github.com/TorchPing/Torch-Web.git:gh-pages

---
+++ precache-manifest.8a57a5a79198aefd0e10d680e62b9d0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560b0bbd659dd3351f4631197c2dbe96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "797c0ae94ad7caf44c1e",
+    "url": "/static/css/2.f1df2cb3.chunk.css"
+  },
+  {
+    "revision": "e805ecfdc1e6ae54f301",
+    "url": "/static/css/main.81f517e2.chunk.css"
+  },
+  {
+    "revision": "797c0ae94ad7caf44c1e",
+    "url": "/static/js/2.95ad1fbd.chunk.js"
+  },
+  {
+    "revision": "e805ecfdc1e6ae54f301",
+    "url": "/static/js/main.51476d4d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
